308673a3b74727ab47f9a0357c446dd4d95a9d12
gavin-feng <gavin.von.feng@gmail.com>
2014-04-02 20:39:23 +0800

    just mod

--- 4/file_server.js
+++ 4/file_server.js
@@ -1,3 +1,9 @@
+var http = require('http')
+var parse = require('url').parse;
+var join = require('path').join;
+var fs = require('fs');
+var root = __dirname;
+
 var server = http.createServer(function(req, res){
   var url = parse(req.url);
   var path = join(root, url.pathname);
@@ -21,3 +27,5 @@ var server = http.createServer(function(req, res){
     }
   });
 });
+
+server.listen(3000);
